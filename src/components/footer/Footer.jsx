9f4f52aef02149668926e3bdca6839635e761ec6
Sayedfarabi <sayedfarabi6099@gmail.com>
2023-04-14 14:44:22 +0600

    navbar and footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer Section</h1>
-        </div>
+        <section>
+            <footer className="footer p-10 bg-neutral text-neutral-content">
+                <div>
+                    <div className='text-left leading-none'>
+                        <Link to={"/"} className="btn btn-ghost normal-case">
+                            <div className='text-left leading-none'>
+                                <h1 className='text-2xl'>User<span>Shop</span></h1>
+                            </div>
+                        </Link>
+                    </div>
+                    <p>Providing reliable service since 1984</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a href='' className="link link-hover">Branding</a>
+                    <a href='' className="link link-hover">Design</a>
+                    <a href='' className="link link-hover">Marketing</a>
+                    <a href='' className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a href='' className="link link-hover">About us</a>
+                    <a href='' className="link link-hover">Contact</a>
+                    <a href='' className="link link-hover">Jobs</a>
+                    <a href='' className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a href='' className="link link-hover">Terms of use</a>
+                    <a href='' className="link link-hover">Privacy policy</a>
+                    <a href='' className="link link-hover">Cookie policy</a>
+                </div>
+            </footer>
+        </section>
     );
 };
 
